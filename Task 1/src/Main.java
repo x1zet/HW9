58159ab2ab6b569eb1c8dcf98a73f9e93ebde2bf
x1zet <155456277+x1zet@users.noreply.github.com>
2024-11-10 16:54:02 +0300

    ready

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -7,30 +9,49 @@ public class Main {
         a = inputDouble();
         b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+        System.out.println("Выберите оператор:\n1) Сложение(+) \n2) Разность(-)\n3) Деление(/)\n4) Умножение(*)");
+        int operator = new Scanner(System.in).nextInt();
+
+        if(operator == 1){
+            sum();
+        } else if (operator == 2) {
+            diff();
+        } else if (operator == 3) {
+            divide();
+        } else if (operator == 4) {
+            multiply();
+        } else{
+            System.out.println("Ошибка");
+        }
     }
 
     private static void divide() {
         //todo дописать логику и печать результата
+        double divide = a/b;
+        System.out.println("Частное равно " + divide);
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        double diff = a-b;
+        System.out.println("Разность равна " + diff);
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        double multiply = a*b;
+        System.out.println("Произведение равно " + multiply);
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        double sum = a+b;
+        System.out.println("Сумма равна " + sum);
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextInt();
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static double firstNumber = inputNumber();
+
     public static void main(String[] args) {
-        //Написать программу, в которой пользователь вводит целое число.
-        //Если число четное, то печатаем true, если не четное, печатаем false
-        //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
-        //Методы и их логику продумайте сами
+        trueOrFalse();
+    }
+
+    private static double inputNumber(){
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextDouble();
+    }
+
+    private static void trueOrFalse(){
+        System.out.println(firstNumber % 2 == 0);
     }
 }
